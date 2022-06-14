4f4bc4dbb5fb4995ff091e21460d29ef3f66d259
lavsora <ya.karpich@gmail.com>
2022-06-15 02:22:31 +0300

    todo list

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+const todoData = []
+
+const render = function () {
+    todoList.innerHTML = ''
+    todoCompleted.innerHTML = ''
+
+    todoData.forEach(function(item) {
+        const li = document.createElement('li')
+
+        li.classList.add('todo-item')
+
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+		    '<button class="todo-remove"></button>' +
+		    '<button class="todo-complete"></button>' + 
+		    '</div>'
+
+        if (item.completed) {
+            todoCompleted.append(li)
+        } else {
+            todoList.append(li)
+        }
+        
+        li.querySelector('.todo-complete').addEventListener('click', function() {
+            item.completed = !item.completed
+            render()
+        })
+
+        li.querySelector('.todo-remove').addEventListener('click', function() {
+            render()
+        })
+    })
+}
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (headerInput.value.trim() === '') {
+        return
+    } else {
+        const newTodo = {
+            text: headerInput.value,
+            completed: false
+        }
+    
+        todoData.push(newTodo)
+        headerInput.value = ''
+    
+        render()
+    }
+})
