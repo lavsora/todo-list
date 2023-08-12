f112613c2e00e67c090b2634123fc01d15590b2b
lavsora <ya.karpich@gmail.com>
2023-08-12 19:52:07 +0300

    adding packages and setting configurations

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,22 +1,23 @@
-import { Component } from "react";
+import { Component } from 'react'
 
-import TasksFilter from "../tasks-filter";
+import TasksFilter from '../tasks-filter'
 
-import './footer.css';
+import './footer.css'
 
 class Footer extends Component {
-    render() {
-        const { countTodo, onDeleted, filter, onFilter } = this.props;
+  render() {
+    const { countTodo, onDeleted, filter, onFilter } = this.props
 
-        return (
-            <footer className="footer">
-                <span className="todo-count">{countTodo > 1 ? `${countTodo} items` : `${countTodo} item` } left</span>
-                <TasksFilter filter={filter} onFilter={(label) => onFilter(label)} />
-                <button className="clear-completed"
-                    onClick={onDeleted}>Clear completed</button>
-            </footer>
-        )
-    }
+    return (
+      <footer className="footer">
+        <span className="todo-count">{countTodo > 1 ? `${countTodo} items` : `${countTodo} item`} left</span>
+        <TasksFilter filter={filter} onFilter={(label) => onFilter(label)} />
+        <button className="clear-completed" onClick={onDeleted}>
+          Clear completed
+        </button>
+      </footer>
+    )
+  }
 }
 
-export default Footer;
+export default Footer
--- src/tasks-filter/tasks-filter.js
+++ src/tasks-filter/tasks-filter.js
@@ -1,34 +1,25 @@
-import { Component } from "react";
+import { Component } from 'react'
 
-import './tasks-filter.css';
+import './tasks-filter.css'
 
 class TasksFilter extends Component {
-    render() {
-        const { filter, onFilter } = this.props;
+  render() {
+    const { filter, onFilter } = this.props
 
-        const buttonsData = [
-            { label: 'All' },
-            { label: 'Active' },
-            { label: 'Completed' }
-        ]
+    const buttonsData = [{ label: 'All' }, { label: 'Active' }, { label: 'Completed' }]
 
-        const buttons = buttonsData.map(({ label }) => {
-            return (
-                <li key={label}>
-                    <button className={label === filter ? 'selected' : ''}
-                        onClick={() => onFilter(label)}>
-                        {label}
-                    </button>
-                </li>
-            )
-        })
+    const buttons = buttonsData.map(({ label }) => {
+      return (
+        <li key={label}>
+          <button className={label === filter ? 'selected' : ''} onClick={() => onFilter(label)}>
+            {label}
+          </button>
+        </li>
+      )
+    })
 
-        return (
-            <ul className="filters">
-                { buttons }
-            </ul>
-        )
-    }
+    return <ul className="filters">{buttons}</ul>
+  }
 }
 
-export default TasksFilter;
+export default TasksFilter
