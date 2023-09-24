c2835925f14e0dfefca8e734224e07e34553c937
lavsora <ya.karpich@gmail.com>
2023-09-24 14:00:07 +0300

    class task-timer => functional task-timer

--- src/task-timer/task-timer.js
+++ src/task-timer/task-timer.js
@@ -1,32 +1,30 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { intervalToDuration } from 'date-fns'
 
 import Icon from '../icon/icon'
 
-class TaskTimer extends PureComponent {
-  captureEvent = (e) => {
+const TaskTimer = ({ playTimer, milisec, done, onToggleTimer }) => {
+  const duration = intervalToDuration({ start: 0, end: milisec })
+
+  const time = [duration.hours, String(duration.minutes).padStart(2, '0'), String(duration.seconds).padStart(2, '0')]
+    .filter(Boolean)
+    .join(':')
+
+  const captureEvent = (e) => {
     e.stopPropagation()
     e.preventDefault()
 
-    this.props.onToggleTimer()
+    onToggleTimer()
   }
 
-  render() {
-    const { playTimer, milisec, done } = this.props
-    const duration = intervalToDuration({ start: 0, end: milisec })
-    const time = [duration.hours, String(duration.minutes).padStart(2, '0'), String(duration.seconds).padStart(2, '0')]
-      .filter(Boolean)
-      .join(':')
-
-    return (
-      <>
-        <button type="button" onClick={this.captureEvent} disabled={done}>
-          <Icon playTimer={playTimer} done={done} />
-        </button>
-        <span className={done ? 'timer-text' : ''}>{time}</span>
-      </>
-    )
-  }
+  return (
+    <>
+      <button type="button" onClick={captureEvent} disabled={done}>
+        <Icon playTimer={playTimer} done={done} />
+      </button>
+      <span className={done ? 'timer-text' : ''}>{time}</span>
+    </>
+  )
 }
 
 export default TaskTimer
